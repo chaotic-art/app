cb096f0b1aec3b493242eadcc2fde5108d0f2d10
hassnian <44554284+hassnian@users.noreply.github.com>
2025-05-26 13:55:36 +0500

    ref: move prefix ref outside useChain function

--- app/composables/useChain.ts
+++ app/composables/useChain.ts
@@ -1,7 +1,8 @@
 import type { Prefix } from '@kodadot1/static'
 
+const prefix = ref<Prefix>('ahp')
+
 export function useChain() {
-  const prefix = ref<Prefix>('ahp')
   const vm = computed(() => vmOf(prefix.value))
 
   return {
