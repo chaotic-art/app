ab0497643bf71bf34272d45b817e16832970eb3e
Jarsen136 <31397967+Jarsen136@users.noreply.github.com>
2025-05-29 15:05:43 +0200

    feat: follow button basic

---
+++ app/components/common/button/ButtonConfig.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+export interface ButtonConfig {
+  label: string
+  icon?: string
+  onClick?: () => void
+  classes?: string
+  active?: boolean
+  disabled?: boolean
+}
+
+defineProps<{
+  button: ButtonConfig
+  loading?: boolean
+  testId?: string
+}>()
+</script>
+
+<template>
+  <UButton
+    rounded
+    no-shadow
+    loading-with-label
+    :data-testid="testId"
+    :class="button.classes"
+    :active="button.active"
+    :disabled="button.disabled || loading"
+    :loading="loading"
+    @click="button.onClick"
+  >
+    <UIcon
+      v-if="button.icon"
+      :name="button.icon"
+      class="mr-1"
+    />
+    {{ button.label }}
+  </UButton>
+</template>
