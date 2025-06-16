2e34d077608336594e0c31ac96faf28f2610ccfa
Jarsen136 <31397967+Jarsen136@users.noreply.github.com>
2025-06-16 22:45:00 +0200

    fix: lint

--- app/components/landing/topCollections/utils/series.ts
+++ app/components/landing/topCollections/utils/series.ts
@@ -1,6 +1,6 @@
 import type { Interaction } from '@/types'
-import { after, between, getVolume } from '@/utils/math'
 import { formatISO, subDays } from 'date-fns'
+import { after, between, getVolume } from '@/utils/math'
 
 export interface SortType {
   field: string
