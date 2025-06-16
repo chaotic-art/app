1b6a128d0d04fc2e4509d12e4dd64404da974dde
Preschian Febryantara <preschian@users.noreply.github.com>
2025-06-16 23:18:08 +0700

    refactor: update error handler to log errors and add Vue error hook

--- app/plugins/error-handler.ts
+++ app/plugins/error-handler.ts
@@ -1,5 +1,10 @@
+/* eslint-disable no-console */
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.config.errorHandler = (error, instance, info) => {
-    console.error({ error, instance, info })
+    console.log({ error, instance, info })
   }
+
+  nuxtApp.hook('vue:error', (error, instance, info) => {
+    console.log({ error, instance, info })
+  })
 })
