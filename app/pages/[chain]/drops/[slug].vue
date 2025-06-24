ae8b0b0f7ac9475a8d00708a6e8cc6b586f6981a
Jarsen136 <31397967+Jarsen136@users.noreply.github.com>
2025-06-24 21:51:39 +0200

    fix: responsive on collect by

--- app/pages/[chain]/drops/[slug].vue
+++ app/pages/[chain]/drops/[slug].vue
@@ -44,7 +44,7 @@ onMounted(async () => {
           {{ collection?.metadata.name ?? '---' }}
         </h1>
 
-        <div class="flex gap-2 justify-between items-center my-6 lg:my-10">
+        <div class="flex flex-col items-start md:flex-row md:items-center gap-4 justify-between my-6 lg:my-10">
           <div class="flex flex-col gap-2">
             <p class="text-sm text-gray-500">
               Created By
