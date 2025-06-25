753e3f87acb34261b88d15db504f5e383f323bfb
hassnian <44554284+hassnian@users.noreply.github.com>
2025-06-25 16:39:39 +0500

    fix: simplify wallet storage using afterHydrate hook

--- app/stores/wallet/index.ts
+++ app/stores/wallet/index.ts
@@ -3,26 +3,6 @@ import type { WalletAccount, WalletExtension, WalletStageType, WalletState } fro
 import { defineStore } from 'pinia'
 import { WalletStageTypes, WalletStates } from './types'
 
-function getStorage() {
-  if (!import.meta.client) {
-    return undefined
-  }
-
-  // remove, backwards compatibility
-  const wallet = localStorage.getItem('wallet')
-
-  if (wallet) {
-    const walletStore = JSON.parse(wallet)
-    const wallets = walletStore.wallets
-    if (Boolean(wallets?.EVM) && Boolean(wallets?.SUB)) {
-      localStorage.setItem('wallet', JSON.stringify({ ...walletStore, wallets: [] }))
-    }
-  }
-  // end backwards compatibility
-
-  return localStorage
-}
-
 export const useWalletStore = defineStore('wallet', () => {
   const isModalOpen = ref(false)
   const wallets = ref<WalletExtension[]>([])
@@ -106,7 +86,14 @@ export const useWalletStore = defineStore('wallet', () => {
 }, {
   persist: {
     // SSR is disabled because wallet components are mostly available only on client side
-    storage: getStorage(),
+    storage: import.meta.client ? localStorage : undefined,
     pick: ['wallets', 'selectedAccounts', 'stage'],
+    // remove, backwards compatibility
+    afterHydrate(context) {
+      if (!Array.isArray(context.store.wallets)) {
+        // @ts-expect-error state unmatch
+        context.store.$patch({ wallets: [] })
+      }
+    },
   },
 })
