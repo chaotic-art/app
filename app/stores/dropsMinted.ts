492e8aef17a2a43d9e440e3de36b63726811168e
Jarsen136 <31397967+Jarsen136@users.noreply.github.com>
2025-06-29 00:13:46 +0200

    feat: drops page classify

---
+++ app/stores/dropsMinted.ts
@@ -0,0 +1,30 @@
+import type { DropItem } from '@/types'
+import { defineStore } from 'pinia'
+
+export const useMintedDropsStore = defineStore('mintedDrops', {
+  state: () => ({
+    mintedDrops: [] as DropItem[],
+  }),
+  getters: {
+    getMintedDrops(): DropItem[] {
+      return this.mintedDrops
+    },
+    sortedMintedDrops(): DropItem[] {
+      return [...this.mintedDrops].sort((a, b) => {
+        // Sort from bigger to lower number
+        return Number(b.collection) - Number(a.collection)
+      })
+    },
+  },
+  actions: {
+    addMintedDrop(drop: DropItem) {
+      const exists = this.mintedDrops.some(d => d.collection === drop.collection)
+      if (!exists) {
+        this.mintedDrops.push(drop)
+      }
+    },
+    clearMintedDrops() {
+      this.mintedDrops = []
+    },
+  },
+})
