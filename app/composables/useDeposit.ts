215c6f0b834fb169b5f7f84cbd4fc7b968796a88
hassnian <44554284+hassnian@users.noreply.github.com>
2025-07-29 14:51:31 +0500

    ref: remove dead code

--- app/composables/useDeposit.ts
+++ app/composables/useDeposit.ts
@@ -1,18 +1,11 @@
 import type { Prefix } from '@kodadot1/static'
-
 import { CHAINS } from '@kodadot1/static'
-// import { balanceOf } from '@kodadot1/sub-api'
-// import { decodeAddress, encodeAddress } from '@polkadot/util-crypto'
 import { chainPropListOf } from '@/utils/chain'
 import format from '@/utils/format/balance'
 
 export default function (prefix: Ref<Prefix>) {
   const { $api } = useNuxtApp()
-
-  // const { accountId } = useAuth()
   const { isAssetHub } = useIsChain(prefix)
-
-  const balance = ref()
 
   const collectionDeposit = ref(0)
   const itemDeposit = ref(0)
@@ -77,28 +70,7 @@ export default function (prefix: Ref<Prefix>) {
     }
   })
 
-  // watchEffect(async () => {
-  //   if (isEnabled.value) {
-  //     const api = $api(prefix.value)
-
-  //     // get chain symbol and decimals
-  //     // const chainInfo = await api.registry.getChainProperties()
-  //     // chainSymbol.value = chainInfo?.tokenSymbol.toHuman()?.[0]
-
-  //     // set balance
-  //     if (accountId.value) {
-  //       const chain = CHAINS[prefix.value]
-  //       const publicKey = decodeAddress(accountId.value)
-  //       const prefixAddress = encodeAddress(publicKey, chain.ss58Format)
-
-  //       // balance.value = await balanceOf(api, prefixAddress)
-  //       // balance.value = format(balance.value, chain.tokenDecimals, false)
-  //     }
-  //   }
-  // })
-
   return {
-    balance,
     collectionDeposit,
     itemDeposit,
     attributeDeposit,
