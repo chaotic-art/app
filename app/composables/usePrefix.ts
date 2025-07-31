835d8210ad02030c4d5fb3a42827dc487c2f0a4c
hassnian <44554284+hassnian@users.noreply.github.com>
2025-07-31 11:43:05 +0500

    feat: allow url prefix

--- app/composables/usePrefix.ts
+++ app/composables/usePrefix.ts
@@ -5,8 +5,13 @@ const prefix = ref<Prefix>('ahp')
 export default function () {
   const client = computed(() => prefix.value)
 
+  const setPrefix = (newPrefix: Prefix) => {
+    prefix.value = newPrefix
+  }
+
   return {
     prefix,
     client,
+    setPrefix,
   }
 }
