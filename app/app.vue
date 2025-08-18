3c52010eaf12584ab54a860b54b65a2cee72aeed
hassnian <44554284+hassnian@users.noreply.github.com>
2025-08-18 07:52:17 +0500

    ref: remove wallet client plugin

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,10 @@
 <script lang="ts" setup>
 const fiatStore = useFiatStore()
+const subWalletStore = useSubWalletStore()
+
+if (import.meta.client) {
+  subWalletStore.init()
+}
 
 onMounted(async () => {
   if (fiatStore.incompleteFiatValues) {
--- app/plugins/wallet.client.ts
+++
@@ -1,5 +0,0 @@
-export default defineNuxtPlugin(() => {
-  const subWalletStore = useSubWalletStore()
-
-  subWalletStore.init()
-})
