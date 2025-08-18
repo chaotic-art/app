bda1df3ed73629e81ebc858aa507dfd6b33f02d3
Preschian Febryantara <preschian@users.noreply.github.com>
2025-08-19 00:46:12 +0700

    feat: enhance token activity and balance handling with dynamic chain specifications

--- app/utils/api/substrate.ts
+++ app/utils/api/substrate.ts
@@ -1,4 +1,5 @@
 import type { SupportedChain } from '~/plugins/sdk.client'
+import { formatBalance } from 'dedot/utils'
 
 export async function getChainSpec(chainPrefix: SupportedChain) {
   const { $sdk } = useNuxtApp()
@@ -11,5 +12,31 @@ export async function getChainSpec(chainPrefix: SupportedChain) {
     decimals: tokenDecimals,
     symbol: tokenSymbol,
     name: chainSpec.name,
+  }
+}
+
+export async function getBalance(chainPrefix: SupportedChain, address: string) {
+  const { $sdk } = useNuxtApp()
+  const { api } = $sdk(chainPrefix)
+
+  const balance = await api.query.System.Account.getValue(address)
+
+  const spec = chainSpec[chainPrefix]
+  const tokenDecimals = spec.tokenDecimals
+  const tokenSymbol = spec.tokenSymbol
+
+  const freeBalance = balance.data.free.toString()
+  const freeBalanceFormatted = formatBalance(freeBalance, { decimals: tokenDecimals, symbol: tokenSymbol })
+
+  const usd = tokenToUsd(Number(freeBalance), tokenDecimals, tokenSymbol)
+  const usdNumeric = usd ? Number(usd.replace('$', '')) : 0
+
+  return {
+    freeBalance,
+    freeBalanceFormatted,
+    tokenName: spec.name,
+    tokenSymbol,
+    usd,
+    usdNumeric,
   }
 }
