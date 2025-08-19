fe5489e6d6381f2a5740403b683c377c849183bc
hassnian <44554284+hassnian@users.noreply.github.com>
2025-08-19 10:48:54 +0500

    styles: listing cart padding

--- app/components/actionCart/ActionCart.vue
+++ app/components/actionCart/ActionCart.vue
@@ -18,7 +18,7 @@ onBeforeUnmount(actionCartStore.clear)
       class="fixed right-24 bottom-9 z-998"
     >
       <div class="inline-flex items-center">
-        <div class="bg-white dark:bg-neutral-800 border border-gray-200 dark:border-neutral-700 flex items-center p-2 gap-8 rounded-2xl">
+        <div class="bg-white dark:bg-neutral-800 border border-gray-200 dark:border-neutral-700 flex items-center p-2 gap-5 rounded-2xl">
           <div class="flex items-center gap-2">
             <div class="px-4">
               <b>{{ actionCartStore.count }}</b>
