7fce490c07fc17154ada697d64a4813091909db8
hassnian <44554284+hassnian@users.noreply.github.com>
2025-08-21 17:18:54 +0500

    ref: type check

--- app/components/shoppingCart/EmptyShoppingCart.vue
+++ app/components/shoppingCart/EmptyShoppingCart.vue
@@ -6,7 +6,7 @@ const { currentMode } = useTheme()
 
 <template>
   <div class="flex items-center flex-col gap-5 pt-8">
-    <img :src="currentMode.value === 'dark' ? '/cart/empty-cart-dark.svg' : '/cart/empty-cart.svg'" alt="Empty Cart" class="w-36 h-36">
+    <img :src="currentMode?.value === 'dark' ? '/cart/empty-cart-dark.svg' : '/cart/empty-cart.svg'" alt="Empty Cart" class="w-36 h-36">
 
     <div class="flex flex-col gap-2 items-center">
       <h1 class="font-bold">
