a1cd83ee4e7a6ee42d71b08eedbf65e248b41a6b
Preschian Febryantara <preschian@users.noreply.github.com>
2025-08-26 22:25:45 +0700

    refactor: move SEO metadata handling from GenerativeDrop to Drops page

--- app/components/drop/GenerativeDrop.vue
+++ app/components/drop/GenerativeDrop.vue
@@ -3,12 +3,6 @@ const { drop, amountToMint } = storeToRefs(useDropStore())
 
 const { decimals, chainSymbol, currentChain } = useChain()
 const { usd: usdPrice, formatted: formattedTokenPrice } = useAmount(computed(() => drop.value?.price), decimals, chainSymbol)
-
-useSeoMeta({
-  title: () => drop.value.name,
-  description: () => drop.value.collectionDescription?.slice(0, 150),
-  ogImage: () => `https://ogi.koda.art/__og-image__/image/${drop.value.chain}/drops/${drop.value.alias}/og.png`, // TODO: at the moment satori somehow doesn't work on cf-pages (defineOgImageComponent)
-})
 </script>
 
 <template>
--- app/pages/[chain]/drops/[slug].vue
+++ app/pages/[chain]/drops/[slug].vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 const dropStore = useDropStore()
-useDrop()
+const { drop } = useDrop()
 
 onBeforeMount(() => dropStore.reset())
 onUnmounted(() => dropStore.reset())
+
+useSeoMeta({
+  title: () => drop.value.name,
+  description: () => drop.value.collectionDescription?.slice(0, 150),
+  ogImage: () => `https://ogi.koda.art/__og-image__/image/${drop.value.chain}/drops/${drop.value.alias}/og.png`, // TODO: at the moment satori somehow doesn't work on cf-pages (defineOgImageComponent)
+})
 </script>
 
 <template>
