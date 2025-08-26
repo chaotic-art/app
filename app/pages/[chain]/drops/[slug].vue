f10f6c0efce10a524fe37b8c55069bf87abeb771
Preschian Febryantara <preschian@users.noreply.github.com>
2025-08-26 22:39:10 +0700

    feat: implement lazy loading for drop data and enhance SEO metadata handling in Drops page

--- app/pages/[chain]/drops/[slug].vue
+++ app/pages/[chain]/drops/[slug].vue
@@ -1,14 +1,37 @@
 <script setup lang="ts">
+import type { AssetHubChain } from '~/plugins/sdk.client'
+import { getDropById } from '~/services/fxart'
+import { fetchOdaCollection } from '~/services/oda'
+
 const dropStore = useDropStore()
-const { drop } = useDrop()
+useDrop()
 
 onBeforeMount(() => dropStore.reset())
 onUnmounted(() => dropStore.reset())
 
+const { params } = useRoute()
+
+// for opengraph purposes only
+const { data: drop } = useLazyAsyncData(
+  `drop-${params.chain}-${params.slug}`,
+  async () => {
+    const drop = await getDropById(params.slug?.toString() ?? '')
+    const collection = await fetchOdaCollection(params.chain?.toString() as AssetHubChain, drop.collection)
+
+    return {
+      ...drop,
+      metadata: {
+        title: collection.metadata?.name,
+        description: collection.metadata?.description,
+      },
+    }
+  },
+)
+
 useSeoMeta({
-  title: () => drop.value.name,
-  description: () => drop.value.collectionDescription?.slice(0, 150),
-  ogImage: () => `https://ogi.koda.art/__og-image__/image/${drop.value.chain}/drops/${drop.value.alias}/og.png`, // TODO: at the moment satori somehow doesn't work on cf-pages (defineOgImageComponent)
+  title: () => drop.value?.metadata?.title,
+  description: () => drop.value?.metadata?.description?.slice(0, 150),
+  ogImage: () => `https://ogi.koda.art/__og-image__/image/${params.chain}/drops/${params.slug}/og.png`, // TODO: at the moment satori somehow doesn't work on cf-pages (defineOgImageComponent)
 })
 </script>
 
