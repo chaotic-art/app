649261470873fd52ca6f4b00fb1edc16b4f1d327
Preschian Febryantara <preschian@users.noreply.github.com>
2025-08-29 16:39:45 +0700

    feat: show drop start time (#381)

--- app/utils/format/time.ts
+++ app/utils/format/time.ts
@@ -1,5 +1,22 @@
-import { formatDistanceToNowStrict } from 'date-fns'
+import { formatDistanceToNowStrict, formatDuration, intervalToDuration } from 'date-fns'
 
 export function formatToNow(date: string | number | Date, addSuffix = true): string {
   return formatDistanceToNowStrict(new Date(date), { addSuffix })
+}
+
+export function formatDetailedTimeToNow(date: string | number | Date): string {
+  const targetDate = new Date(date)
+  const now = new Date()
+  const isPast = targetDate.getTime() < now.getTime()
+
+  const duration = intervalToDuration({
+    start: isPast ? targetDate : now,
+    end: isPast ? now : targetDate,
+  })
+
+  const formattedDuration = formatDuration(duration, {
+    format: ['days', 'hours', 'minutes'],
+  })
+
+  return formattedDuration ? (isPast ? `${formattedDuration} ago` : `in ${formattedDuration}`) : 'now'
 }
