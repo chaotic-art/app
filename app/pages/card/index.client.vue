4845e3125fb43c7d986dba3071098373d362bf10
Jarsen136 <31397967+Jarsen136@users.noreply.github.com>
2025-09-20 14:32:45 +0200

    feat: mint card page init

---
+++ app/pages/card/index.client.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+definePageMeta({
+  title: 'Mint Card',
+  layout: 'empty',
+})
+
+const { setColorMode, currentMode } = useTheme()
+const { doAfterLogin } = useDoAfterlogin()
+
+// only dark mode for this page
+watch(currentMode, () => {
+  setColorMode('dark')
+}, { immediate: true })
+
+onUnmounted(() => {
+  setColorMode('system')
+})
+
+function handleClaimClick() {
+  doAfterLogin({
+    onLoginSuccess: () => {
+      // TODO: Implement claim functionality
+    },
+  })
+}
+</script>
+
+<template>
+  <div class="min-h-full flex flex-col overflow-hidden bg-black">
+    <LazyNavbar />
+    <MintCard @claim="handleClaimClick" />
+    <LazyFooter />
+  </div>
+</template>
