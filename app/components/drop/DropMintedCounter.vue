9e41abf257587656827d75af7a29dec4f250f20a
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2025-09-25 22:08:24 +0200

    feat: Minted out drop: show its minted out (#513)

---
+++ app/components/drop/DropMintedCounter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { DropItem } from '@/types'
+
+defineProps<{
+  drop: DropItem
+}>()
+</script>
+
+<template>
+  <span class="font-medium text-foreground flex items-center">{{ drop?.minted }}/
+    <UIcon v-if="drop?.max && drop?.max >= Number.MAX_SAFE_INTEGER" name="mdi:infinity" class="w-4 h-4" />
+    <span v-else>{{ drop?.max }}</span>
+
+    <span class="ml-1">{{ drop?.isMintedOut ? 'Minted Out' : $t('drop.minted') }}</span>
+  </span>
+</template>
