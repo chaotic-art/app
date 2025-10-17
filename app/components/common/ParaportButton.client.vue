ba2f00f9d993c9e6944c1ff991a8f692f0b6e118
hassnian <44554284+hassnian@users.noreply.github.com>
2025-10-17 08:49:59 +0500

    log

--- app/components/common/ParaportButton.client.vue
+++ app/components/common/ParaportButton.client.vue
@@ -75,9 +75,13 @@ onBeforeMount(async () => {
 
   const result = await pingStore.getFastestEndpoints(paraportEndpoints.chains)
 
+  console.log(result)
+
   Object.entries(result).forEach(([chain, chainEndpoints]) => {
     endpoints.value[chain as ChaoticSupportedChain] = chainEndpoints
   })
+
+  console.log(endpoints.value)
 
   fetched.value = true
 })
