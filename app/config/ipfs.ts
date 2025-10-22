0bb61254b93438191bb820b33a7ec1fdcdc0e359
Preschian Febryantara <preschian@users.noreply.github.com>
2025-10-22 14:34:23 +0700

    refactor: update IPFS provider type from 'image' to 'chaotic' for consistency

--- app/config/ipfs.ts
+++ app/config/ipfs.ts
@@ -1,11 +1,11 @@
 export const CF_IMAGE_URL = 'https://imagedelivery.net/Im3azVCMHMp2rDcvZOACIg/'
 export const CHAOTIC_BUCKET_URL = 'https://bucket.chaotic.art/'
 
-export type IPFSProviders = 'ipfs' | 'image'
+export type IPFSProviders = 'ipfs' | 'chaotic'
 
 const ipfsProviders: Partial<Record<IPFSProviders, string>> = {
   ipfs: 'https://ipfs.io/',
-  image: CHAOTIC_BUCKET_URL,
+  chaotic: CHAOTIC_BUCKET_URL,
 }
 
 export function getIPFSProvider(providerName: IPFSProviders): string {
