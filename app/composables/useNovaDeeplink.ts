124b3235b7d620f1840afc8648f78e455cfb1193
hassnian <44554284+hassnian@users.noreply.github.com>
2025-10-28 12:02:07 +0500

    fix: better is in mobile wallet tracking

--- app/composables/useNovaDeeplink.ts
+++ app/composables/useNovaDeeplink.ts
@@ -1,10 +1,14 @@
-import { SubstrateWalletSources } from '~/utils/wallet/substrate/types'
+import { MobileWalletList } from '~/utils/wallet/substrate/config'
 
 export default function () {
   const { isLogIn } = useAuth()
   const { wallets } = storeToRefs(useWalletStore())
 
-  const isAvailable = computed(() => !isLogIn.value && isMobileDevice && !wallets.value.find(wallet => wallet.id === SubstrateWalletSources.Nova)?.installed)
+  const isInMobileWalletBrowser = computed(() => wallets.value
+    .filter(wallet => (MobileWalletList as string[]).includes(wallet.id))
+    .some(wallet => wallet.installed) && isMobileDevice)
+
+  const isAvailable = computed(() => !isLogIn.value && !isInMobileWalletBrowser.value)
 
   const redirect = (path = window.location.pathname) => {
     window.open(`https://app.novawallet.io/open/dapp?url=https://beta.chaotic.art${path}`, '_blank')
