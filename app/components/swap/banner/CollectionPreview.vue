21204cf5be6294a9bee4c080cb3a0cfeed106fe6
hassnian <44554284+hassnian@users.noreply.github.com>
2025-11-13 21:11:30 +0500

    fix(swaps): collection preview styles

--- app/components/swap/banner/CollectionPreview.vue
+++ app/components/swap/banner/CollectionPreview.vue
@@ -11,8 +11,8 @@ const { collection } = useOdaCollection(computed(() => props.collectionId))
 </script>
 
 <template>
-  <div class="p-1 bg-secondary inline-block rounded-full">
-    <NuxtLink v-if="collection" :to="`/${currentChain}/collection/${collectionId}`" class="flex items-center gap-2 rounded-full w-fit min-w-0 bg-secondary hover:bg-ring">
+  <div class="p-1 bg-secondary inline-block rounded-full h-[60px]">
+    <NuxtLink v-if="collection" :to="`/${currentChain}/collection/${collectionId}`" class="flex items-center gap-2 rounded-full w-fit h-full min-w-0 bg-secondary hover:bg-ring px-2">
       <div class="rounded-full overflow-hidden bg-background shrink-0 h-10 aspect-square p-[3px]">
         <img :src="sanitizeIpfsUrl(collection.metadata?.image)" sizes="40px" :title="collection.metadata?.name" class="object-cover rounded-full h-full w-full">
       </div>
