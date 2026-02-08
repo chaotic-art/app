5bfce79997f07e2db828ca12a275f6d296067bac
Hassnian <44554284+hassnian@users.noreply.github.com>
2026-02-08 14:31:53 +0500

    fix: free mint item deposit race condition  (#737)
    * fix: free mint item deposit paraport reace condition issue
    
    * ref: remove dead code

--- app/composables/useDropMinimumFunds.ts
+++ app/composables/useDropMinimumFunds.ts
@@ -1,13 +1,15 @@
 export function useDropMinimumFunds() {
   const { drop } = useDrop()
   const { currentChain } = useChain()
-  const { itemDeposit } = useDeposit(currentChain)
   const { amountToMint: amount } = storeToRefs(useDropStore())
+  const { itemDeposit, loading: depositLoading } = useDeposit(currentChain)
 
   const price = computed<number>(() => Number(drop.value?.price) || 0)
   const minimumFunds = computed<number>(() => amount.value * (price.value + itemDeposit.value))
+  const loading = computed(() => depositLoading.value)
 
   return {
     minimumFunds,
+    loading,
   }
 }
