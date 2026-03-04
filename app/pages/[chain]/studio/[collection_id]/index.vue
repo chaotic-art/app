a438a32033eebe3e53930a400ab29a781101022e
Jarsen136 <31397967+Jarsen136@users.noreply.github.com>
2026-03-04 22:29:04 +0100

    feat: studio page major layout

--- app/pages/[chain]/studio/[collection_id]/index.vue
+++ app/pages/[chain]/studio/[collection_id]/index.vue
@@ -2,18 +2,14 @@
 import { isAssetHubChain } from '~/utils/chain'
 
 definePageMeta({
+  layout: 'studio-collection',
   validate: async (route) => {
     const { chain } = route.params
     return typeof chain === 'string' && isAssetHubChain(chain)
   },
 })
-
-useSeoMeta({
-  title: 'Manage Collection - Studio',
-  description: 'Manage your NFT collection.',
-})
 </script>
 
 <template>
-  <UContainer class="px-4 py-8 md:px-6" />
+  <div />
 </template>
