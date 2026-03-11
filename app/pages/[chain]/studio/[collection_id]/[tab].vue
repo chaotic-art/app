739af6fe063e0426b99aecd3740e191d9c7754cb
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2026-03-11 20:01:49 +0100

    feat: collection edit on studio details page (#848)

--- app/pages/[chain]/studio/[collection_id]/[tab].vue
+++ app/pages/[chain]/studio/[collection_id]/[tab].vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import CollectionHeader from '~/components/collection/CollectionHeader.vue'
 import { isAssetHubChain } from '~/utils/chain'
 
 const validTabs = ['preview', 'details', 'items'] as const
@@ -30,6 +31,11 @@ const { collection } = useOdaCollection(collectionId)
       :collection="collection ?? null"
       :collection-id="collectionId"
       :chain="currentChain"
+    />
+    <StudioCollectionDetails
+      v-else-if="tab === 'details'"
+      :collection-id="collectionId"
+      :collection="collection ?? null"
     />
     <p
       v-else
