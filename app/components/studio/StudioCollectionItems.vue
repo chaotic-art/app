296dcaf629f9695f351f0c4d9a71a9fee2195e8b
Jarsen136 <31397967+Jarsen136@users.noreply.github.com>
2026-03-12 23:35:07 +0100

    feat: add toolbar for collection items in studio

--- app/components/studio/StudioCollectionItems.vue
+++ app/components/studio/StudioCollectionItems.vue
@@ -1,37 +1,14 @@
 <script setup lang="ts">
 import type { AssetHubChain } from '~/plugins/sdk.client'
-import { normalizeRarityTotalItems } from '~/types/rarity'
 
 const props = defineProps<{
   collectionId: string
   chain: AssetHubChain
 }>()
 
-const collectionIdRef = toRef(props, 'collectionId')
-const { collection } = useOdaCollection(collectionIdRef)
-
-const { buildOrderBy, sortOptions } = useSortOptions('collectionItems')
-const { viewMode: nftViewMode, gridClass: nftGridClass } = useNftViewMode('collection')
-
-const defaultSortKeys = computed(() => [sortOptions.value[0]?.value ?? 'recent'])
-const queryVariables = computed(() => ({
-  collections: [props.collectionId],
-  orderBy: buildOrderBy(defaultSortKeys.value),
-}))
-
-const collectionRarityTotalItems = computed(() =>
-  normalizeRarityTotalItems(collection.value?.supply),
-)
+const { accountId } = useAuth()
 </script>
 
 <template>
-  <LazyNftsGrid
-    :variables="queryVariables"
-    :grid-class="nftGridClass"
-    :view-mode="nftViewMode"
-    no-items-found-message="This collection doesn't have any items yet."
-    :prefix="chain"
-    :rarity-total-items="collectionRarityTotalItems"
-    show-rarity
-  />
+  <ProfileNftsList hide-chain-switcher :extra-variables="{ owner: accountId, collections: [props.collectionId] }" />
 </template>
