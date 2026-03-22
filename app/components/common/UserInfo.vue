bef99acba2054a68f7ab3de75dbebc3b9a570dc4
hassnian <44554284+hassnian@users.noreply.github.com>
2026-03-22 09:47:16 +0500

    feat(wallet): disable evm wallet profile siderbar

--- app/components/common/UserInfo.vue
+++ app/components/common/UserInfo.vue
@@ -7,12 +7,14 @@ const props = withDefaults(
     avatarSize?: number
     transparentBackground?: boolean
     customName?: boolean
+    openProfileOnClick?: boolean
   }>(),
   {
     address: '',
     avatarSize: 40,
     customName: false,
     transparentBackground: false,
+    openProfileOnClick: true,
   },
 )
 
@@ -23,12 +25,23 @@ const { profile: profileFromAddress } = useFetchProfile(
 )
 
 const name = computed(() => profileFromAddress.value?.name || shortenAddress(props.address))
+
+const linkComponent = computed(() => props.openProfileOnClick ? 'NuxtLink' : 'div')
+const linkProps = computed(() => props.openProfileOnClick ? { to: `/${currentChain}/u/${props.address}` } : {})
 </script>
 
 <template>
-  <NuxtLink :to="`/${currentChain}/u/${address}`" class="flex items-center gap-2 rounded-full w-fit min-w-0 bg-secondary hover:bg-border" :class="{ 'bg-transparent!': transparentBackground, 'px-2 py-1.5': !transparentBackground }">
+  <component
+    :is="linkComponent"
+    v-bind="linkProps"
+    class="flex items-center gap-2 rounded-full w-fit min-w-0 bg-secondary hover:bg-border"
+    :class="{
+      'bg-transparent!': transparentBackground,
+      'px-2 py-1.5': !transparentBackground,
+    }"
+  >
     <ProfileAvatar :address="address" :size="avatarSize" />
     <slot name="name" :address-name="name" :description="profileFromAddress?.description" />
     <span v-if="!customName" class="text-ellipsis overflow-hidden whitespace-nowrap">{{ name }}</span>
-  </NuxtLink>
+  </component>
 </template>
--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -2,9 +2,14 @@ import { accountsAreSame } from '@/utils/account'
 
 export default function () {
   const accountStore = useAccountStore()
+  const walletStroe = useWalletStore()
 
   const accountId = computed(() => accountStore.address || '')
   const isLogIn = computed(() => Boolean(accountId.value))
+
+  const defaultAccount = computed(() => {
+    return accountId.value || walletStroe.getSelectedAccountAddress('EVM')
+  })
 
   const isCurrentAccount = (address?: string) => accountsAreSame(accountId.value, address)
 
@@ -12,5 +17,6 @@ export default function () {
     accountId,
     isLogIn,
     isCurrentAccount,
+    defaultAccount,
   }
 }
