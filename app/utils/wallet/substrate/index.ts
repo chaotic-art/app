19542043a23404c9f013ffb690c99accd714306f
hassnian <44554284+hassnian@users.noreply.github.com>
2026-03-22 10:21:42 +0500

    ref: remove unused function

--- app/utils/wallet/substrate/index.ts
+++ app/utils/wallet/substrate/index.ts
@@ -1,5 +1,4 @@
 import type { SubstrateWalletSource } from './types'
-import { decodeAddress, keccakAsHex } from 'dedot/utils'
 
 export function getInjectedExtension(source: SubstrateWalletSource) {
   const injectedWindow = window
@@ -8,9 +7,4 @@ export function getInjectedExtension(source: SubstrateWalletSource) {
 
 export function isExtensionInstalled(source: SubstrateWalletSource): boolean {
   return Boolean(getInjectedExtension(source))
-}
-
-export function derieveEthAddress(address: string): `0x${string}` {
-  const hash = keccakAsHex(decodeAddress(address))
-  return `0x${hash.slice(-40)}` as `0x${string}`
 }
