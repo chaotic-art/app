4280760eafcfe4e96c6968ad6c387cf8ee1810ed
hassnian <44554284+hassnian@users.noreply.github.com>
2026-03-22 09:57:29 +0500

    fix: typo

--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -2,13 +2,13 @@ import { accountsAreSame } from '@/utils/account'
 
 export default function () {
   const accountStore = useAccountStore()
-  const walletStroe = useWalletStore()
+  const walletStore = useWalletStore()
 
   const accountId = computed(() => accountStore.address || '')
   const isLogIn = computed(() => Boolean(accountId.value))
 
   const defaultAccount = computed(() => {
-    return accountId.value || walletStroe.getSelectedAccountAddress('EVM')
+    return accountId.value || walletStore.getSelectedAccountAddress('EVM')
   })
 
   const isCurrentAccount = (address?: string) => accountsAreSame(accountId.value, address)
