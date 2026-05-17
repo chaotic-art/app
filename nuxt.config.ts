91999cbc04f4355b3e265e6009631126ad5aa2fa
Viki Val <viktorko99@gmail.com>
2026-05-17 19:52:29 +0200

    :wrech: modern config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import process from 'node:process'
+import { fileURLToPath } from 'node:url'
 import { publicEnv } from './env.public'
 
 const posthogProjectId = process.env.POSTHOG_PROJECT_ID || ''
@@ -10,6 +11,43 @@ const posthogSourcemapsEnabled = process.env.NODE_ENV === 'production'
   && Boolean(posthogPersonalApiKey)
 
 export default defineNuxtConfig({
+  alias: {
+    'polkadot-api/polkadot-sdk-compat': fileURLToPath(new URL('./app/utils/polkadotSdkCompat.ts', import.meta.url)),
+    'polkadot-api/ws-provider/node': fileURLToPath(new URL('./app/utils/polkadotWsProvider.ts', import.meta.url)),
+    'polkadot-api/ws-provider/web': fileURLToPath(new URL('./app/utils/polkadotWsProvider.ts', import.meta.url)),
+    'polkadot-api/ws-provider': fileURLToPath(new URL('./app/utils/polkadotWsProvider.ts', import.meta.url)),
+  },
+  vite: {
+    resolve: {
+      alias: [
+        {
+          find: /^polkadot-api$/,
+          replacement: fileURLToPath(new URL('./app/utils/polkadotApiCompat.ts', import.meta.url)),
+        },
+        {
+          find: /^polkadot-api\/polkadot-sdk-compat$/,
+          replacement: fileURLToPath(new URL('./app/utils/polkadotSdkCompat.ts', import.meta.url)),
+        },
+        {
+          find: /^polkadot-api\/ws-provider(?:\/(?:web|node))?$/,
+          replacement: fileURLToPath(new URL('./app/utils/polkadotWsProvider.ts', import.meta.url)),
+        },
+      ],
+    },
+    optimizeDeps: {
+      include: [
+        '@apollo/client',
+        '@kodadot1/static',
+        '@tanstack/vue-query',
+        'dedot/utils',
+        'eventemitter3',
+        'jdenticon',
+        'polkadot-api',
+        'viem',
+        'vue-dompurify-html',
+      ],
+    },
+  },
   devtools: { enabled: true },
   sourcemap: {
     client: 'hidden',
@@ -134,7 +172,7 @@ export default defineNuxtConfig({
   },
 
   pinia: {
-    storesDirs: ['./app/stores/**'],
+    storesDirs: ['stores', 'stores/**'],
   },
 
   i18n: {
